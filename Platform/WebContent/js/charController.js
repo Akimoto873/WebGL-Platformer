600572d67764005672473f2a571dd52999c16b77
Akimoto873 <>
2015-11-28 21:26:21 +0100

    Code cleanup! - Removing double tabs, removing empty line spaces, adding empty line spaces to make it readable by humans, etc.

--- Platform/WebContent/js/charController.js
+++ Platform/WebContent/js/charController.js
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 var runForward = false;
 var walkForward = false;
 var walkBackward = false;
@@ -16,62 +12,71 @@ var force = 300;
 
 //checks keyinputs on every loop turn.
 function checkKeys() {
-	if(timerNotRunning){
-		
-		timerNotRunning = false;
-	}
-	if (keyMap[87]) { //W
-		if (keyMap[16] //Shift = run
-				&& stamina > 0 && Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
-						+ Math.pow(charMesh.getLinearVelocity().z, 2)) < 8
-				) {
-			runForward = true;
-		} else if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2) //else walk
-				+ Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
-			walkForward = true;
-		}
-	}
-	if (!keyMap[16] && stamina < 200) { //if not running. Regen stamina.
-		stamina += 1;
-	}
-	if (keyMap[83]) { // S - walk backwards
-		if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
-				+ Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
-			walkBackward = true;
-		}
-	}
-	if (keyMap[65]) { // A
-		counterClockwiseRotation = 2; // charMesh.setAngularVelocity(new
-										// THREE.Vector3(0, 1.5, 0));
+    if(timerNotRunning){
+            timerNotRunning = false;
+    }
 
-	}
-	if (keyMap[68]) { // D
-		clockwiseRotation = -2; // charMesh.setAngularVelocity(new
-									// THREE.Vector3(0, -1.5, 0));
-	}
+    // W
+    if (keyMap[87]) { 
+        //Shift = run
+        if (keyMap[16] && stamina > 0 
+            && Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2) 
+            + Math.pow(charMesh.getLinearVelocity().z, 2)) < 8) {
+                runForward = true;
+        } else if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2) //else walk
+            + Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
+                walkForward = true;
+        }
+    }
 
-	if (keyMap[69]) { // Q - strafe
-		if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
-				+ Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
-			strafeLeft = true;
-		}
-	}
+    // If not running. Regen stamina.
+    if (!keyMap[16] && stamina < 200) { 
+            stamina += 1;
+    }
 
-	if (keyMap[81]) { // E - strafe
+    // S - walk backwards
+    if (keyMap[83]) { 
+            if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
+                + Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
+                    walkBackward = true;
+            }
+    }
 
-		if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
-				+ Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
-			strafeRight = true;
-		}
-	}
-	if (keyMap[82] && gameOverScreen) { //Restart level is dead and R is pressed.
-		restartLevel();
-	}
+    // A
+    if (keyMap[65]) { 
+            counterClockwiseRotation = 2; // charMesh.setAngularVelocity(new
+    }
 
+    // D
+    if (keyMap[68]) { 
+            clockwiseRotation = -2; // charMesh.setAngularVelocity(new    // THREE.Vector3(0, -1.5, 0));
+    }
+
+    // Q - strafe
+    if (keyMap[69]) { 
+            if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
+                            + Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
+                    strafeLeft = true;
+            }
+    }
+
+    // E - strafe
+    if (keyMap[81]) { 
+
+            if (Math.sqrt(Math.pow(charMesh.getLinearVelocity().x, 2)
+                            + Math.pow(charMesh.getLinearVelocity().z, 2)) < 4) {
+                    strafeRight = true;
+            }
+    }
+
+    // Restart level is dead and R is pressed.
+    if (keyMap[82] && gameOverScreen) { 
+            restartLevel();
+    }
 }
 
 
-//Handles all character movement.
+// Handles all character movement.
 function checkMovement() {
 	
 //	var oldVelocityVector = charMesh.getLinearVelocity();
@@ -85,241 +90,235 @@ function checkMovement() {
 //	}
 //	var force = 10*(4 - oldVelocity)/deltaT; //Force needed this frame to simulate setlinearvelocity
 //	var runForce = 10*(8 - oldVelocityRun)/deltaT;
-	if (runForward) {
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var forceVector = new THREE.Vector3(0, 0, force );
-		var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
-		var oldVelocityVector = charMesh.getLinearVelocity();
-		charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
-				finalForceVector.z));
-		stamina -= 2;
-	} else if (walkForward) {
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var forceVector = new THREE.Vector3(0, 0, force);
-		var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
-		
-		charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
-				finalForceVector.z));
-		
-	}
+    // W + shift is pressed
+    if (runForward) {
+            var rotationMatrix = new THREE.Matrix4();
+            rotationMatrix.extractRotation(charMesh.matrix);
+            var forceVector = new THREE.Vector3(0, 0, force );
+            var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
+            var oldVelocityVector = charMesh.getLinearVelocity();
+            charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
+                            finalForceVector.z));
+            stamina -= 2;
+    } 
+    
+    // W is pressed
+    else if (walkForward) {
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.extractRotation(charMesh.matrix);
+        var forceVector = new THREE.Vector3(0, 0, force);
+        var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
+        charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0, finalForceVector.z));
+    }
 
-	if (walkBackward) { // S
+    // S is pressed
+    if (walkBackward) { 
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.extractRotation(charMesh.matrix);
+        var forceVector = new THREE.Vector3(0, 0, -force);
+        var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
+        var oldVelocityVector = charMesh.getLinearVelocity();
+        charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0, finalForceVector.z));
+    }
+    
+    // Space is pressed
+    if (jump) { 
+        if(airborne1 || airborne2){
+            airTime = new THREE.Clock();
+            charMesh.applyCentralImpulse(new THREE.Vector3(0, 60, 0));
+            stamina -= 20;
+            // health -= 10;
+            // damaged = true; //for testing purposes
+        }
+    }
+    
+    charMesh.setAngularVelocity(new THREE.Vector3(0, clockwiseRotation + counterClockwiseRotation, 0));
 
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var forceVector = new THREE.Vector3(0, 0, -force);
-		var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
-		var oldVelocityVector = charMesh.getLinearVelocity();
-		charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
-				finalForceVector.z));
+    // Q is pressed
+    if (strafeLeft) { 
 
-	}
-	if (jump) { // Space
-		if(airborne1 || airborne2){
-			airTime = new THREE.Clock();
-			charMesh.applyCentralImpulse(new THREE.Vector3(0, 60, 0));
-			stamina -= 20;
-			// health -= 10;
-			// damaged = true; //for testing purposes
-		}
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.extractRotation(charMesh.matrix);
+        var forceVector = new THREE.Vector3(-force, 0, 0);
+        var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
+        var oldVelocityVector = charMesh.getLinearVelocity();
+        charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0, finalForceVector.z));
+    }
 
-	}
+    // E is pressed
+    if (strafeRight) { 
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.extractRotation(charMesh.matrix);
+        var forceVector = new THREE.Vector3(force, 0, 0);
+        var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
+        var oldVelocityVector = charMesh.getLinearVelocity();
+        charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0, finalForceVector.z));
+    }
 
-	charMesh.setAngularVelocity(new THREE.Vector3(0, clockwiseRotation
-			+ counterClockwiseRotation, 0));
+    if (pickup) {
+        for (var i = 0; i < pickUpItems.length; i++){
+            var distance = new THREE.Vector3();
+            distance.subVectors(charMesh.position, pickUpItems[i].position);
+            
+            if (distance.length() < 3) {
+                scene.remove(pickUpItems[i]);
+                    
+                if(pickUpItems[i] == crate && !carrying) {
+                        crate.position.x = 0;
+                        crate.position.y = -1;
+                        crate.position.z = 0;
+                        charMesh.add(crate);
+                        crate.position.z += 1;
+                        carrying = true;
+                        pickupThisFrame = true;
+                        i = pickUpItems.length + 1;
+                        pickUpItems.splice(i, 1);
+                }
+                else if(pickUpItems[i] == cones){
+                        pickUpItems.splice(i, 1);
+                        carriedCones += 5;
+                        i = pickUpItems.length + 1;
+                }
+                else {
+                        pickUpItems.splice(i, 1);
+                        carriedCones += 1;
+                        log(carriedCones);
+                        i = pickUpItems.length + 1;
+                }
+            }
+        }
+        
+        if (carrying && !pickupThisFrame) {
+            charMesh.remove(crate);
+            var rotationMatrix = new THREE.Matrix4();
+            rotationMatrix.extractRotation(charMesh.matrix);
+            var positionDiff = new THREE.Vector3(0, 0, 1);
+            var finalPosition = positionDiff.applyMatrix4(rotationMatrix);
+            var oldPosition = charMesh.position
+            crate.position.x = oldPosition.x + finalPosition.x;
+            crate.position.y = oldPosition.y - 1;
+            crate.position.z = oldPosition.z + finalPosition.z;
+            scene.add(crate);
+            pickUpItems.push(crate);
+            carrying = false;
+        }
+    }
 
-	if (strafeLeft) { // Q
+    checkTraps();
+    checkFallDmg();
 
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var forceVector = new THREE.Vector3(-force, 0, 0);
-		var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
-		var oldVelocityVector = charMesh.getLinearVelocity();
-		charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
-				finalForceVector.z));
-	}
-
-	if (strafeRight) { // E
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var forceVector = new THREE.Vector3(force, 0, 0);
-		var finalForceVector = forceVector.applyMatrix4(rotationMatrix);
-		var oldVelocityVector = charMesh.getLinearVelocity();
-		charMesh.applyCentralForce(new THREE.Vector3(finalForceVector.x, 0,
-				finalForceVector.z));
-	}
-
-	if (pickup) {
-		
-			for (var i = 0; i < pickUpItems.length; i++){
-				var distance = new THREE.Vector3();
-				distance.subVectors(charMesh.position, pickUpItems[i].position);
-				if (distance.length() < 3) {
-					scene.remove(pickUpItems[i]);
-					if(pickUpItems[i] == crate && !carrying) {
-						crate.position.x = 0;
-						crate.position.y = -1;
-						crate.position.z = 0;
-						charMesh.add(crate);
-						crate.position.z += 1;
-						carrying = true;
-						pickupThisFrame = true;
-						i = pickUpItems.length + 1;
-						pickUpItems.splice(i, 1);
-					}
-					else if(pickUpItems[i] == cones){
-						pickUpItems.splice(i, 1);
-						carriedCones += 5;
-						i = pickUpItems.length + 1;
-					}
-					else {
-						pickUpItems.splice(i, 1);
-						carriedCones += 1;
-						log(carriedCones);
-						i = pickUpItems.length + 1;
-					}
-					
-					
-				}
-		}
-
-		if (carrying && !pickupThisFrame) {
-			charMesh.remove(crate);
-			var rotationMatrix = new THREE.Matrix4();
-			rotationMatrix.extractRotation(charMesh.matrix);
-			var positionDiff = new THREE.Vector3(0, 0, 1);
-			var finalPosition = positionDiff.applyMatrix4(rotationMatrix);
-			var oldPosition = charMesh.position
-			crate.position.x = oldPosition.x + finalPosition.x;
-			crate.position.y = oldPosition.y - 1;
-			crate.position.z = oldPosition.z + finalPosition.z;
-			scene.add(crate);
-			pickUpItems.push(crate);
-			carrying = false;
-
-		}
-
-	}
-
-	checkTraps();
-	checkFallDmg();
-
-	if (health < 1 && !gameOverScreen) {
-		showGameOver();
-	}
-	
-	
-
+    // If we have lost all our health, set game over
+    if (health < 1 && !gameOverScreen) {
+            showGameOver();
+    }
 }
 
 //Checks if traps are triggered, and handles what happens then.
 function checkTraps() {
-	
+    if (charMesh._physijs.touches.indexOf(tile._physijs.id) === 1 || crate._physijs.touches.indexOf(tile._physijs.id) === 1) {
+        if (!triggered) {
+            scene.remove(trap);
+            scene.add(trap);
+            trap.setLinearFactor(new THREE.Vector3(0, 1, 0));
+            trap.setAngularFactor(new THREE.Vector3(0, 0, 0));
+            triggered = true;
+            trapTime = new THREE.Clock();
 
-	if (charMesh._physijs.touches.indexOf(tile._physijs.id) === 1 || crate._physijs.touches.indexOf(tile._physijs.id) === 1) {
-		if (!triggered) {
-			scene.remove(trap);
-			scene.add(trap);
-			trap.setLinearFactor(new THREE.Vector3(0, 1, 0));
-			trap.setAngularFactor(new THREE.Vector3(0, 0, 0));
-			triggered = true;
-			trapTime = new THREE.Clock();
-			
-		}
-	}
-	if (triggered) {
-		log(applyForce);
-		log(trap.position.y);
+        }
+    }
+    if (triggered) {
+        log(applyForce);
+        log(trap.position.y);
 
-		if (trap.position.y < 2) {
-			applyForce = true;
-		}
-		if (applyForce) {
-			trap.applyCentralForce(new THREE.Vector3(0, 1100, 0));
-			
-		}
-		if (trap.position.y > 4 && trapTime.getElapsedTime() > 2) {
-			trap.setLinearVelocity(new THREE.Vector3(0, 0, 0));
-			trap.setLinearFactor(new THREE.Vector3(0, 0, 0));
-			applyForce = false;
+        if (trap.position.y < 2) {
+                applyForce = true;
+        }
+        
+        if (applyForce) {
+                trap.applyCentralForce(new THREE.Vector3(0, 1100, 0));
 
-		}
+        }
+        
+        if (trap.position.y > 4 && trapTime.getElapsedTime() > 2) {
+                trap.setLinearVelocity(new THREE.Vector3(0, 0, 0));
+                trap.setLinearFactor(new THREE.Vector3(0, 0, 0));
+                applyForce = false;
 
-		if (trapTime.getElapsedTime() > 10) {
-			trapTime.stop();
-			triggered = false;
-		}
-	}
+        }
 
-	var intersects = trapCaster2.intersectObjects(moveableObjects);
-	if (intersects.length > 0) {
+        if (trapTime.getElapsedTime() > 10) {
+                trapTime.stop();
+                triggered = false;
+        }
+    }
 
-		if (!triggered2) {
+    var intersects = trapCaster2.intersectObjects(moveableObjects);
+    if (intersects.length > 0) {
+        if (!triggered2) {
 
-			trap2.setLinearVelocity(new THREE.Vector3(0, 0, -7));
-			triggered2 = true;
+            trap2.setLinearVelocity(new THREE.Vector3(0, 0, -7));
+            triggered2 = true;
 
-		}
-	}
+        }
+    }
 }
 
-//Checks if the character should take fall dmg.
+// Checks if the character should take fall dmg.
 function checkFallDmg() {
-	charCaster.set(charMesh.position, new THREE.Vector3(0, -1, 0));
-	var intersects = charCaster.intersectObjects(objects);
-	if ((airborne1 || airborne2) && airTime.getElapsedTime() > 1) { // check for landing
+    charCaster.set(charMesh.position, new THREE.Vector3(0, -1, 0));
+    var intersects = charCaster.intersectObjects(objects);
+    if ((airborne1 || airborne2) && airTime.getElapsedTime() > 1) { // check for landing
 
-		for (var i = 0; i < intersects.length; i++) {
-			if (intersects[i].distance < 10) {
-				airborne1 = false;
-				airborne2 = false;
-				airTime.stop();
+            for (var i = 0; i < intersects.length; i++) {
+                    if (intersects[i].distance < 10) {
+                            airborne1 = false;
+                            airborne2 = false;
+                            airTime.stop();
 
-			}
-		}
-	}
-	if (intersects[0]) {
-		if (charMesh.getLinearVelocity().y < -14 && intersects[0].distance < 2) {
-			if (fallClock.getElapsedTime() > 1) {
-				var damage = Math.abs(charMesh.getLinearVelocity().y) * 1.5;
-				health -= damage;
-				damaged = true;
-				fallClock = new THREE.Clock();
-			}
-		}
-	}
+                    }
+            }
+    }
+    if (intersects[0]) {
+            if (charMesh.getLinearVelocity().y < -14 && intersects[0].distance < 2) {
+                    if (fallClock.getElapsedTime() > 1) {
+                            var damage = Math.abs(charMesh.getLinearVelocity().y) * 1.5;
+                            health -= damage;
+                            damaged = true;
+                            fallClock = new THREE.Clock();
+                    }
+            }
+    }
 }
+
 
 function dropCone(){
-	if (carriedCones > 0){
-		carriedCones -= 1;
-		var rotationMatrix = new THREE.Matrix4();
-		rotationMatrix.extractRotation(charMesh.matrix);
-		var positionDiff = new THREE.Vector3(0, 0, 1);
-		var finalPosition = positionDiff.applyMatrix4(rotationMatrix);
-		var oldPosition = charMesh.position
-		var cone = new Physijs.BoxMesh(coneGeometry, Physijs.createMaterial(new THREE.MeshBasicMaterial({color: 0xff8800}), 1, .1), 5);
-		cone.scale.set(0.2, 0.2, 0.2);
-		cone.position.x = oldPosition.x + finalPosition.x;
-		cone.position.y = oldPosition.y - 1;
-		cone.position.z = oldPosition.z + finalPosition.z;
-		scene.add(cone);
-		pickUpItems.push(cone);
-	}
+    if (carriedCones > 0){
+        carriedCones -= 1;
+        var rotationMatrix = new THREE.Matrix4();
+        rotationMatrix.extractRotation(charMesh.matrix);
+        var positionDiff = new THREE.Vector3(0, 0, 1);
+        var finalPosition = positionDiff.applyMatrix4(rotationMatrix);
+        var oldPosition = charMesh.position
+        var cone = new Physijs.BoxMesh(coneGeometry, Physijs.createMaterial(new THREE.MeshBasicMaterial({color: 0xff8800}), 1, .1), 5);
+        cone.scale.set(0.2, 0.2, 0.2);
+        cone.position.x = oldPosition.x + finalPosition.x;
+        cone.position.y = oldPosition.y - 1;
+        cone.position.z = oldPosition.z + finalPosition.z;
+        scene.add(cone);
+        pickUpItems.push(cone);
+    }
 }
 
 //resets all values before next run through.
 function resetValues() {
-	runForward = false;
-	walkForward = false;
-	walkBackward = false;
-	counterClockwiseRotation = 0;
-	clockwiseRotation = 0;
-	strafeLeft = false;
-	strafeRight = false;
-	jump = false;
-	pickupThisFrame = false;
-	pickup = false;
+    runForward = false;
+    walkForward = false;
+    walkBackward = false;
+    counterClockwiseRotation = 0;
+    clockwiseRotation = 0;
+    strafeLeft = false;
+    strafeRight = false;
+    jump = false;
+    pickupThisFrame = false;
+    pickup = false;
 }
--- Platform/WebContent/js/hudController.js
+++ Platform/WebContent/js/hudController.js
@@ -1,67 +1,64 @@
-/**
- * 
- */
-
-
-
 //Creates the health and stamina bars.
-function createOverlay() {
-	
+function createOverlay() 
+{
+    // Create and append overlay container
+    overlayContainer = document.createElement('div');
+    document.body.appendChild(overlayContainer);
 
-	healthTexture = textureLoader.load('images/health.jpg');
-	healthBoxTexture = textureLoader.load('images/healthBox.png');
-	
+    // Textures
+    healthTexture = textureLoader.load('images/health.jpg');
+    healthBoxTexture = textureLoader.load('images/healthBox.png');
 
-	
-	overlayContainer = document.createElement('div');
-	document.body.appendChild(overlayContainer);
+    // Health Bar Texture
+    var spriteMaterial = new THREE.SpriteMaterial({
+            map : healthTexture,
+            color : 0x00ff00
+    });
+    healthSprite = new THREE.Sprite(spriteMaterial);
+    healthSprite.position.set(-(window.innerWidth / 3.2), -(window.innerHeight / 2) + 100, 10);
+    healthSprite.scale.set(window.innerWidth / 3, window.innerHeight / 16, 1);
+    orthoScene.add(healthSprite);
 
-	
-	var spriteMaterial = new THREE.SpriteMaterial({
-		map : healthTexture,
-		color : 0x00ff00
-	});
-	healthSprite = new THREE.Sprite(spriteMaterial);
-	healthSprite.position.set(-(window.innerWidth / 3.2),
-			-(window.innerHeight / 2) + 100, 10);
-	healthSprite.scale.set(window.innerWidth / 3, window.innerHeight / 16, 1);
-	orthoScene.add(healthSprite);
-	var spriteMaterial2 = new THREE.SpriteMaterial({
-		map : healthBoxTexture,
-		color : 0x000000
-	});
-	healthSprite2 = new THREE.Sprite(spriteMaterial2);
-	healthSprite2.position.set(-(window.innerWidth / 3.2),
-			-(window.innerHeight / 2) + 100, 8);
-	healthSprite2.scale
-			.set(window.innerWidth / 2.8, window.innerHeight / 15, 1);
-	orthoScene.add(healthSprite2);
-	var spriteMaterial3 = new THREE.SpriteMaterial({
-		map : healthTexture,
-		color : 0x0000ff
-	});
-	staminaSprite = new THREE.Sprite(spriteMaterial3);
-	staminaSprite.position.set(-(window.innerWidth / 3.2),
-			-(window.innerHeight / 2.5) + 100, 10);
-	staminaSprite.scale.set(window.innerWidth / 3, window.innerHeight / 16, 1);
-	orthoScene.add(staminaSprite);
-	var spriteMaterial4 = new THREE.SpriteMaterial({
-		map : healthBoxTexture,
-		color : 0x000000
-	});
-	staminaSprite2 = new THREE.Sprite(spriteMaterial4);
-	staminaSprite2.position.set(-(window.innerWidth / 3.2),
-			-(window.innerHeight / 2.5) + 100, 8);
-	staminaSprite2.scale.set(window.innerWidth / 2.8, window.innerHeight / 15,
-			1);
-	orthoScene.add(staminaSprite2);
+    // Health Box Texture
+    var spriteMaterial2 = new THREE.SpriteMaterial({
+            map : healthBoxTexture,
+            color : 0x000000
+    });
+    healthSprite2 = new THREE.Sprite(spriteMaterial2);
+    healthSprite2.position.set(-(window.innerWidth / 3.2), -(window.innerHeight / 2) + 100, 8);
+    healthSprite2.scale.set(window.innerWidth / 2.8, window.innerHeight / 15, 1);
+    orthoScene.add(healthSprite2);
+
+    // Stamina Bar Texture
+    var spriteMaterial3 = new THREE.SpriteMaterial({
+            map : healthTexture,
+            color : 0x0000ff
+    });
+    staminaSprite = new THREE.Sprite(spriteMaterial3);
+    staminaSprite.position.set(-(window.innerWidth / 3.2), -(window.innerHeight / 2.5) + 100, 10);
+    staminaSprite.scale.set(window.innerWidth / 3, window.innerHeight / 16, 1);
+    orthoScene.add(staminaSprite);
+
+    // Stamina Box Texture
+    var spriteMaterial4 = new THREE.SpriteMaterial({
+            map : healthBoxTexture,
+            color : 0x000000
+    });
+    staminaSprite2 = new THREE.Sprite(spriteMaterial4);
+    staminaSprite2.position.set(-(window.innerWidth / 3.2), -(window.innerHeight / 2.5) + 100, 8);
+    staminaSprite2.scale.set(window.innerWidth / 2.8, window.innerHeight / 15, 1);
+    orthoScene.add(staminaSprite2);
 }
 
-//Shows the game over screen on death.
+/* Shows the game over screen on death */
 function showGameOver() {
+    
+        // Textures
 	bloodTexture = textureLoader.load('images/blood.jpg');
 	gameOverTexture = textureLoader.load('images/gameOver.jpg');
 	restartTexture = textureLoader.load('images/restart.jpg');
+        
+        // Blood Material (splash on screen)
 	var bloodMaterial = new THREE.SpriteMaterial({
 		map : bloodTexture,
 		opacity : 0.1
@@ -70,6 +67,8 @@ function showGameOver() {
 	bloodSprite.position.set(0, 0, 5);
 	bloodSprite.scale.set(window.innerWidth, window.innerHeight, 1);
 	orthoScene.add(bloodSprite);
+        
+        // Game Over Screen
 	var gameOverMaterial = new THREE.SpriteMaterial({
 		map : gameOverTexture,
 		opacity : 0.0
@@ -79,68 +78,68 @@ function showGameOver() {
 	gameOverSprite.scale.set(window.innerWidth, window.innerHeight, 1);
 	orthoScene.add(gameOverSprite);
 
+        // Press R to restart message screen
 	var restartSpriteMaterial = new THREE.SpriteMaterial({
 		map : restartTexture,
 		opacity : 0.0
 	});
 	restartSprite = new THREE.Sprite(restartSpriteMaterial);
 	restartSprite.position.set(0, 0, 7);
-	restartSprite.scale.set(window.innerWidth / 2.5, window.innerHeight / 2.5,
-			1);
+	restartSprite.scale.set(window.innerWidth / 2.5, window.innerHeight / 2.5, 1);
 	orthoScene.add(restartSprite);
+        
+        
 	gameOverScreen = true;
 	gameOverAudio.play();
 }
 
-//Scales the health and stamina bars based on health and stamina remaining.
+// Scales the health and stamina bars based on health and stamina remaining.
 function checkChangesToHUD() {
+    staminaSprite.scale.set((Math.abs(stamina) / 200) * (window.innerWidth / 3), window.innerHeight / 16, 1);
+    staminaSprite.position.x = (-window.innerWidth / 3.2) - (1 - Math.abs(stamina / 200)) * (window.innerWidth / 6.0);
 
-	staminaSprite.scale.set(
-			(Math.abs(stamina) / 200) * (window.innerWidth / 3),
-			window.innerHeight / 16, 1);
-	staminaSprite.position.x = (-window.innerWidth / 3.2)
-			- (1 - Math.abs(stamina / 200)) * (window.innerWidth / 6.0);
+    // If you have been hurt, we update the apperance of your health
+    if (damaged) {
+        // Update the size of the health bar according to your health
+        healthSprite.scale.set((Math.abs(health) / 100) * (window.innerWidth / 3), window.innerHeight / 16, 1);
+        healthSprite.position.x = (-window.innerWidth / 3.2) - (1 - Math.abs(health / 100)) * (window.innerWidth / 6.0);
+        
+        // Color codes your health bar according to amount of health
+        if (health > 80) {
+                healthSprite.material.color.setHex(0x00ff00);   // Green
+        }
+        if (health < 80) {
+                healthSprite.material.color.setHex(0xffff00);   // Yellow
+        }
+        if (health < 50) {
+                healthSprite.material.color.setHex(0xff0000);   // Red
+        }
+        
+        damaged = false;
+    }
 
-	if (damaged) {
-		healthSprite.scale.set((Math.abs(health) / 100)
-				* (window.innerWidth / 3), window.innerHeight / 16, 1);
-		healthSprite.position.x = (-window.innerWidth / 3.2)
-				- (1 - Math.abs(health / 100)) * (window.innerWidth / 6.0);
-		if (health > 80) {
-			healthSprite.material.color.setHex(0x00ff00);
-		}
-		if (health < 80) {
-			healthSprite.material.color.setHex(0xffff00);
-		}
-		if (health < 50) {
-			healthSprite.material.color.setHex(0xff0000);
-		}
-		damaged = false;
-	}
-
-	if (gameOverScreen) {
-		if (bloodSprite.material.opacity < 0.8) {
-
-			bloodSprite.material.opacity += 0.01;
-			gameOverSprite.material.opacity += 0.015;
-		} else if (restartSprite.material.opacity < 0.5) {
-			restartSprite.material.opacity += 0.02;
-		}
-	}
-
+    // Fading in the game over screen(s)
+    if (gameOverScreen) {
+        if (bloodSprite.material.opacity < 0.8) {
+                bloodSprite.material.opacity += 0.01;
+                gameOverSprite.material.opacity += 0.015;
+        } else if (restartSprite.material.opacity < 0.5) {
+                restartSprite.material.opacity += 0.02;
+        }
+    }
 }
 
-//removes the menu
+// Removes the menu
 function removeMenu(){
-	menu = false;
-	menuSprite.visible = false;
-	playSprite.visible = false;
-	optionsSprite.visible = false;
+    menu = false;
+    menuSprite.visible = false;
+    playSprite.visible = false;
+    optionsSprite.visible = false;
 }
-//shows the menu
+// Shows the menu
 function showMenu(){
-	menu = true;
-	menuSprite.visible = true;
-	playSprite.visible = true;
-	optionsSprite.visible = true;
+    menu = true;
+    menuSprite.visible = true;
+    playSprite.visible = true;
+    optionsSprite.visible = true;
 }
